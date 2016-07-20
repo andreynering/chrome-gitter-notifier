2993fe4d31e4c6da7afae282545f8c42254a27d1
Andrey Nering <andrey.nering@gmail.com>
2016-07-20 20:37:40 -0300

    Fetch and show unread messages count from Gitter API

--- background.js
+++ background.js
@@ -1,18 +1,60 @@
-var ba = chrome.browserAction;
-
-function setAllRead() {
-  ba.setBadgeText({text: ''});
+function removeBadge() {
+  chrome.browserAction.setBadgeText({text: ''});
 }
-
-function setUnread(unreadItemCount) {
-  ba.setBadgeBackgroundColor({color: [255, 0, 0, 128]});
-  ba.setBadgeText({text: '' + unreadItemCount});
+function setBadge(text) {
+  chrome.browserAction.setBadgeBackgroundColor({color: '#ff0000'});
+  chrome.browserAction.setBadgeText({text: ''+text});
+}
+function setTitle(text) {
+  chrome.browserAction.setTitle({title: ''+text});
 }
 
 function func() {
-  setUnread(new Date().getMinutes());
+  chrome.storage.sync.get(null, function(options) {
+    if (!options.token) {
+      setBadge('E');
+      setTitle('API token not set\nPlease, see the options page');
+      return;
+    }
+
+    removeBadge();
+
+    var request = new XMLHttpRequest(),
+      unreadCount = 0,
+      titles = [];
+    request.open('GET', 'https://api.gitter.im/v1/rooms', true);
+    request.onload = function() {
+      if (this.status < 200 || this.status >= 400) {
+        setBadge('E');
+        setTitle('Error: could not connect');
+        return;
+      }
+      var rooms = JSON.parse(this.response);
+      var length = rooms.length;
+      for (var i = 0; i < length; i++) {
+        var room = rooms[i];
+        if (room.unreadCount > 0) {
+          unreadCount += room.unreadCount;
+          titles.push(room.unreadCount+' messages on '+room.name);
+        }
+      }
+      if (unreadCount > 0) {
+        setBadge(unreadCount);
+        setTitle(titles.join('\n'));
+      } else {
+        removeBadge();
+        setTitle('No unread messages');
+      }
+    };
+    request.onerror = function() {
+      setBadge('E');
+      setTitle('Error: could not connect');
+    }
+    request.setRequestHeader('Authorization', 'Bearer '+options.token);
+    request.send();
+  });
 }
 
+func();
 chrome.alarms.onAlarm.addListener(func);
 chrome.alarms.create('time', {periodInMinutes: 1});
-func();
